8a509cd0225a1e0fa75b93bcf7d94578a6cc047d
TDICK54312 <gteldirith543@gmail.com>
2015-05-13 10:09:40 -0500

    Started the main page

---
+++ Models/aboutme.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TDICK
+ * Date: 5/13/2015
+ * Time: 8:44 AM
+ */
--- Models/index.php
+++ Models/index.php
@@ -10,10 +10,30 @@
 <html>
     <head>
         <title>Tim Dickson</title>
-
+        <link rel="stylesheet" href="../CSS/index.css">
     </head>
     <body>
-        
+        <div id="master-holder">
+            <div id="banner">
+                <div id="banner-logo" class="noselect">
+                    <div id="first-title">
+                        Tim Dickson
+                    </div>
+                </div>
+                <div id="banner-rest">
+                    <div id="nav-bar">
+                        <a href="index.php">Home</a>
+                        <a href="aboutme.php">About Me</a>
+                    </div>
+                </div>
+            </div>
+            <div id="page-banner">
+                <div id="site-page">Home</div>
+            </div>
+            <div id="content">
+
+            </div>
+        </div>
     </body>
 </html>
 
